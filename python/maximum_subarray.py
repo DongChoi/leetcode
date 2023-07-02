67fa73e1a883a936827ee5bd4846830a1c434af7
DongChoi <dongandrewchoi@gmail.com>
2023-07-02 16:46:48 -0700

    adds maximum subarray

---
+++ python/maximum_subarray.py
@@ -0,0 +1,15 @@
+"""checks if previous sum is bigger than the prevsum + current value and if it 
+is smaller, reset"""
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]
+        curSum = 0
+
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+            maxSub = max(maxSub, curSum)
+        return maxSub
